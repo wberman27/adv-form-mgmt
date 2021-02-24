43bbe35afdaf199899b4af66e24c158376c19a9e
Will B <wberman27@gmail.com>
2021-02-24 15:17:30 -0500

    added structuring to App and Form components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,69 @@
+import React, {useState, useEffect} from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Form from './Form';
+import axios from 'axios'
+import * as yup from 'yup'
+
+const initialFormValues = {
+  //text input
+  name: '',
+  email: '',
+  password: '',
+  //checkbox
+  tos: false,
+}
+
+const initialFormErrors = {
+  name: '',
+  email: '',
+  password: '',
+  tos: false,
+}
+const initialUsers = []
+const initialDisabled = false
 
 function App() {
+
+  const [users, setUsers] = useState(initialUsers)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)
+
+  const getUsers = () => {
+    axios.get('http://myapi.com/api/Users')
+      .then(res =>{
+        setUsers(res.data)
+      })
+      .catch(err =>{
+        console.log(err)
+      })
+  }
+
+  const postNewUser = newUser => {
+    // 🔥 STEP 6- IMPLEMENT! ON SUCCESS ADD NEWLY CREATED User TO STATE
+    //    helper to [POST] `newUser` to `http://buddies.com/api/Users`
+    //    and regardless of success or failure, the form should reset
+    axios.post('http://myapi.com/api/Users', newUser)
+      .then(res =>{
+        setUsers([res.data, ...users])
+      })
+      .catch(err =>{
+        console.log(err)
+      })
+      setFormValues(initialFormValues)
+  }
+
   return (
     <div className="App">
-      <Form />
+      Hi from App
+      <Form 
+      values={formValues}
+      submit={formSubmit}
+      change={inputChange}
+      disabled={disabled}
+      errors={formErrors}
+      />
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,88 @@
-function Form() {
+import React from 'react'
 
-}
+function Form(props) {
+    const {
+      values,
+      submit,
+      change,
+      disabled,
+      errors,
+    } = props
+  
+    const onSubmit = evt => {
+      evt.preventDefault()
+      submit()
+    }
+  
+    const onChange = evt => {
+      const { name, value, type, checked } = evt.target
+      const valueToUse = type === 'checkbox' ? checked : value
+      change(name, valueToUse)
+    }
+  
+    return (
+      <form className='form container' onSubmit={onSubmit}>
+        <div className='form-group submit'>
+          <h2>Add User</h2>
+  
+          <button disabled = {disabled}>submit</button>
+  
+          <div className='errors'>
+            <div>{errors.name}</div>
+            <div>{errors.email}</div>
+            <div>{errors.password}</div>
+            <div>{errors.tos}</div>
+          </div>
+        </div>
+  
+        <div className='form-group inputs'>
+          <h4>Info</h4>
+  
+          <label>Name
+            <input
+              value={values.name}
+              onChange={onChange}
+              name='name'
+              type='text'
+            />
+          </label>
+  
+          <label>Email
+            <input
+              value={values.email}
+              onChange={onChange}
+              name='email'
+              type='text'
+            />
+          </label>
+
+          <label>Password
+            <input
+              value={values.password}
+              onChange={onChange}
+              name= 'password'
+              type='text'
+            />
+          </label>
+  
+
+        </div>
+  
+        <div className='form-group checkboxes'>
+  
+          <label>Terms of Service
+            <input 
+              type='checkbox'
+              name='tos'
+              onChange={onChange}
+              checked={values.tos} 
+            />
+          </label>
+  
+        </div>
+      </form>
+    )
+  }
+  
 
 export default Form;
